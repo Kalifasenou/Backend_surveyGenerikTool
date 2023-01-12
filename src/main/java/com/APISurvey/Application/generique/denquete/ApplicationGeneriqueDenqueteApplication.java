438f92e67ebbd29739f4d6b20deaa5430cef8976
Kalifa SENOU <kalifasenou10@gmail.com>
2023-01-12 10:27:43 +0000

    Ajout de dependance du JPA

--- src/main/java/com/APISurvey/Application/generique/denquete/ApplicationGeneriqueDenqueteApplication.java
+++ src/main/java/com/APISurvey/Application/generique/denquete/ApplicationGeneriqueDenqueteApplication.java
@@ -3,6 +3,7 @@ package com.APISurvey.Application.generique.denquete;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApplicationGeneriqueDenqueteApplication {
 
