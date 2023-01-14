d0847f272e79b3639102cd3bf756b467f65ff299
Kalifa SENOU <kalifasenou10@gmail.com>
2023-01-14 05:24:27 +0000

    Ajout de l'annotation EnableSchelduling pour permettre à l'api d'executer une tache periodiquement sans l'intervention d'une tierce personne

--- src/main/java/com/APISurvey/Application/generique/denquete/ApplicationGeneriqueDenqueteApplication.java
+++ src/main/java/com/APISurvey/Application/generique/denquete/ApplicationGeneriqueDenqueteApplication.java
@@ -2,9 +2,11 @@ package com.APISurvey.Application.generique.denquete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class ApplicationGeneriqueDenqueteApplication {
 
 	public static void main(String[] args) {
